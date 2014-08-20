59eaac8c8f1eef5eaae05c2434140d93cdffbecb
addtheletters <bzhang.2003@gmail.com>
2014-08-19 22:26:26 -0400

    Movewin, trying rain

--- Assets/TextBolt.cs
+++ Assets/TextBolt.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TextBolt : MonoBehaviour {
 	
@@ -10,21 +11,21 @@ public class TextBolt : MonoBehaviour {
 
 		public TextCell( Vector3 pos, int size, int font_res, float scale, Color color, GameObject managers, 
 		                string fontname = "Pixelate", string materialname = "Font Material"){
-			text_mesh = new GameObject("text cell");
+			text_mesh = new GameObject("Text Cell");
 			text_mesh.transform.position = pos;
-			TextMesh mesh_component = (TextMesh)(text_mesh.AddComponent("TextMesh"));
+			TextMesh mesh_component = (TextMesh)(text_mesh.AddComponent<TextMesh>());
 
 			mesh_component.fontSize 	 = font_res;
 			mesh_component.characterSize = size;
 			mesh_component.color 		 = color;
 			mesh_component.text 		 = getText();
 
-			FontManager fm = (FontManager)(managers.GetComponent("FontManager"));
+			FontManager fm = (FontManager)(managers.GetComponent<FontManager>());
 			mesh_component.font 		 = (fm).get.GetFont(fontname);
 
 
 			mr = text_mesh.transform.GetComponent<MeshRenderer>();
-			MaterialManager mm = (MaterialManager)(managers.GetComponent("MaterialManager"));
+			MaterialManager mm = (MaterialManager)(managers.GetComponent<MaterialManager>());
 			mr.material = (mm).get.GetMaterial(materialname);
 
 			/*if (mr.material == null){
@@ -33,11 +34,11 @@ public class TextBolt : MonoBehaviour {
 
 			text_mesh.transform.localScale = text_mesh.transform.localScale * scale;
 			Vector3 rot = text_mesh.transform.eulerAngles;
-			rot.z -= 90;
+			rot.z -= 90; //undynamic. Perhaps change to be dependent on move direction?
 			text_mesh.transform.eulerAngles = rot;
 		}
 
-		public string getText(string charChoices = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM?!@#$%^&*()_+-={}[]|:;<>"){
+		public string getText(string charChoices = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM?!@#$%^&*()+-={}[]|:;<>"){
 			//a single random character
 			return ""+(charChoices[ Random.Range(0, charChoices.Length) ]);
 			//return "UNIMPLEMENTED";
@@ -62,49 +63,51 @@ public class TextBolt : MonoBehaviour {
 			return true;
 		}
 	}
-	
-	//public Component fm;
 
-	public int SIZE = 5;
-	public int FONT_RES = 10;
-	public Font FONT;
+	public int SIZE = 1;
+	public int FONT_RES = 100;
+	public float SCALE = 0.1f;	
+	public Color BASE_COLOR = new Color(71f/255f, 255f/255f, 103f/255f);
+	
+	public GameObject MANAGERS; // object containing Font and Material manager components
+	// must be passed in initialization or through editor
+
 	public float MOVE_SPEED = 1f;
-	public float SCALE = 0.1f;
 	public Vector3 MOVE_DIR = Vector3.down; //should have magnitude 1
 	
-	public double ALPHA_FADE = 0.001; // how much alpha decreases every frame
-	public Color BASE_COLOR = new Color(1, 1, 1);
-
-	float CHAR_SPACING = 1f;
+	public double ALPHA_FADE = 0.01; // how much alpha decreases every frame
+	
+	public float CHAR_SPACING = 0.7f; // this ought to be dynamic at some point. later
 	Vector3 original_pos;
 
-	ArrayList cells = new ArrayList();
-	public GameObject MANAGERS;
+	public bool smooth_terminate = false; // set to true and the bolt will end / kill itself once the existing cells fade
 
-
-	//initialization
+	List<TextCell> cells = new List<TextCell>();
+	int cellcounter = 0;
+	
 	void Start () {
-		//FONT = fm.get.GetFont("Pixelate");
 		original_pos = transform.position;
-		//AddCell ();
 	}
 
 	void AddCell(){
 		cells.Add( new TextCell (transform.position, SIZE, FONT_RES, SCALE, BASE_COLOR, MANAGERS) );
+		cellcounter ++;
 	}
 
-	//called once per frame
-	void Update () {
-		Vector3 deltaPos = transform.position - original_pos;
-		if (deltaPos.magnitude > cells.Count * CHAR_SPACING){
-
-			print ("mag:"+deltaPos.magnitude);
-			print ("space:"+cells.Count * CHAR_SPACING);
-
-			AddCell();
+	void FixedUpdate () {
+		if (!smooth_terminate) {
+			Vector3 deltaPos = transform.position - original_pos;
+			if (deltaPos.magnitude > (cellcounter) * CHAR_SPACING) {
+				AddCell ();
+			}
 		}
 		Recolor ();
 		Move ();
+		if (smooth_terminate) {
+			if(cells.Count == 0){
+				Destroy(gameObject);
+			}
+		}
 	}
 
 	void Recolor(){
@@ -121,8 +124,11 @@ public class TextBolt : MonoBehaviour {
 	}
 
 	void Move(){
-		// Hah! I used fixedDeltaTime like a serious programmer dude!
 		transform.Translate(MOVE_DIR * MOVE_SPEED * Time.fixedDeltaTime);
+	}
+
+	public void SmoothTerminate(){
+		smooth_terminate = true;
 	}
 
 }
---
+++ Assets/TextRainer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TextRainer : MonoBehaviour {
+
+	public float frequency = 1; //bolts per tick
+	public int max_bolts = 50;
+
+	public GameObject MANAGERS; // needs to be passed via editor or initialization
+	
+	public float xSpread = 5;
+	public float zSpread = 5;
+	public float ySpread = 0;
+
+	List<GameObject> bolts = new List<GameObject>();
+	int bolt_counter  = 0;
+	
+	void AddBolt(){
+		Vector3 spawnPos = transform.position;
+		spawnPos.x += xSpread * Random.value - xSpread/2;
+		spawnPos.y += ySpread * Random.value - ySpread/2;
+		spawnPos.z += zSpread * Random.value - zSpread/2;
+		AddBolt (spawnPos);
+	}
+
+	void AddBolt(Vector3 startPos){
+		GameObject bolt = new GameObject ("Text Bolt");
+		bolt.transform.position = startPos;
+		bolts.Add (bolt);
+
+		TextBolt tb = (TextBolt)(bolt.AddComponent<TextBolt>());
+		tb.MANAGERS = MANAGERS;
+
+		bolt_counter ++;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		AddBolt (transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while (bolts.Count < max_bolts) {
+			AddBolt();
+		}
+		foreach(GameObject bolt in bolts){
+			if(outsideCameraView(bolt)){
+				bolt.GetComponent<TextBolt>().SmoothTerminate();
+			}
+		}
+	}
+
+	bool outsideCameraView(GameObject obj){
+		return false;
+	}
+}
